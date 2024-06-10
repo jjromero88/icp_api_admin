60a280d8e2f110f8e0422566b933a2c156f12e9a
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-06-10 18:52:36 -0500

    Feat: implement Transversal.Common project

---
+++ PCM.SIP.ICP/PCM.SIP.ICP.Transversal.Common/Common/ApplicationKeys.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace PCM.SIP.ICP.Transversal.Common
+{
+    public static class ApplicationKeys
+    {
+
+        private static IConfigurationRoot _config;
+        static ApplicationKeys()
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            _config = builder.Build();
+        }
+
+        public static string UrlValidateToken => _config["Config:UrlValidateToken"];
+    }
+}
