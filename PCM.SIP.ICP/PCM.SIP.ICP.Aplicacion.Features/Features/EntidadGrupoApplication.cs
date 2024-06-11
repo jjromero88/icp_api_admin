57d5b490b6a0b7bc1a644d6d132c04aa1e1fe53b
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-06-10 19:58:35 -0500

    Feat: Implement Aplicacion project

---
+++ PCM.SIP.ICP/PCM.SIP.ICP.Aplicacion.Features/Features/EntidadGrupoApplication.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using PCM.SIP.ICP.Aplicacion.Dto;
+using PCM.SIP.ICP.Aplicacion.Interface;
+using PCM.SIP.ICP.Aplicacion.Validator;
+using PCM.SIP.ICP.Transversal.Common;
+using PCM.SIP.ICP.Transversal.Common.Generics;
+
+namespace PCM.SIP.ICP.Aplicacion.Features
+{
+    public class EntidadGrupoApplication : IEntidadGrupoApplication
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly IAppLogger<EntidadGrupoApplication> _logger;
+        private readonly EntidadGrupoValidationManager _perfilValidationManager;
+
+        public EntidadGrupoApplication(
+            IUnitOfWork unitOfWork, 
+            IMapper mapper, 
+            IAppLogger<EntidadGrupoApplication> logger, 
+            EntidadGrupoValidationManager perfilValidationManager)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _logger = logger;
+            _perfilValidationManager = perfilValidationManager;
+        }
+
+        public PcmResponse Insert(Request<EntidadGrupoDto> request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PcmResponse Update(Request<EntidadGrupoDto> request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PcmResponse Delete(Request<EntidadGrupoDto> request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PcmResponse GetById(Request<EntidadGrupoDto> request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PcmResponse GetList(Request<EntidadGrupoDto> request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PCM.SIP.ICP/PCM.SIP.ICP.Aplicacion.Features/Features/IEntidadGrupoApplication.cs
@@ -0,0 +1,15 @@
+﻿using PCM.SIP.ICP.Aplicacion.Dto;
+using PCM.SIP.ICP.Transversal.Common;
+using PCM.SIP.ICP.Transversal.Common.Generics;
+
+namespace PCM.SIP.ICP.Aplicacion.Features
+{
+    public interface IEntidadGrupoApplication
+    {
+        PcmResponse Delete(Request<EntidadGrupoDto> request);
+        PcmResponse GetById(Request<EntidadGrupoDto> request);
+        PcmResponse GetList(Request<EntidadGrupoDto> request);
+        PcmResponse Insert(Request<EntidadGrupoDto> request);
+        PcmResponse Update(Request<EntidadGrupoDto> request);
+    }
+}
