6e617f824c1dfa0f31a5fce47c0fd2a0a507e2b1
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-06-10 19:34:12 -0500

    Feat: Implement Application.dto project

---
+++ PCM.SIP.ICP/PCM.SIP.ICP.Aplicacion.Dto/Dto/EntidadGrupoDto.cs
@@ -0,0 +1,43 @@
+﻿
+namespace PCM.SIP.ICP.Aplicacion.Dto
+{
+    public class EntidadGrupoDto : EntidadBase
+    {
+        public string? codigo { get; set; }
+        public string? abreviatura { get; set; }
+        public string? descripcion { get; set; }
+    }
+    public class EntidadGrupoIdRequest
+    {
+        public string? SerialKey { get; set; }
+    }
+    public class EntidadGrupoInsertRequest
+    {
+        public string? codigo { get; set; }
+        public string? abreviatura { get; set; }
+        public string? descripcion { get; set; }
+    }
+    public class EntidadGrupoUpdateRequest
+    {
+        public string? SerialKey { get; set; }
+        public string? codigo { get; set; }
+        public string? abreviatura { get; set; }
+        public string? descripcion { get; set; }
+    }
+    public class EntidadGrupoFilterRequest
+    {
+        public string? SerialKey { get; set; }
+        public string? filtro { get; set; }
+    }
+    public class EntidadGrupoResponse
+    {
+        public string? SerialKey { get; set; }
+        public string? codigo { get; set; }
+        public string? abreviatura { get; set; }
+        public string? descripcion { get; set; }
+    }
+    public class EntidadGrupoListResponse
+    {
+        public List<EntidadGrupoResponse>? Lista { get; set; }
+    }
+}
