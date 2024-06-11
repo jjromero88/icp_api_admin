555853c70cc2d9e07572ca3c28a058c65a44f743
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-06-10 19:36:53 -0500

    Feat: Implement Aplicacion.Interface.Features for IEntidadGrupoApplication

--- PCM.SIP.ICP/PCM.SIP.ICP.Aplicacion.Interface/Features/IEntidadGrupoApplication.cs
+++ PCM.SIP.ICP/PCM.SIP.ICP.Aplicacion.Interface/Features/IEntidadGrupoApplication.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PCM.SIP.ICP.Transversal.Common.Generics;
+using PCM.SIP.ICP.Transversal.Common;
+using PCM.SIP.ICP.Aplicacion.Dto;
 
 namespace PCM.SIP.ICP.Aplicacion.Interface.Features
 {
     public interface IEntidadGrupoApplication
     {
+        PcmResponse Insert(Request<EntidadGrupoDto> request);
+        PcmResponse Update(Request<EntidadGrupoDto> request);
+        PcmResponse Delete(Request<EntidadGrupoDto> request);
+        PcmResponse GetById(Request<EntidadGrupoDto> request);
+        PcmResponse GetList(Request<EntidadGrupoDto> request);
     }
 }
