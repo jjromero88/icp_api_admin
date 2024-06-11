8d703042bae1d26c2ad4728db8a9b2c957fcd7be
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-06-10 19:53:34 -0500

    Feat: Implement validator project

---
+++ PCM.SIP.ICP/PCM.SIP.ICP.Aplicacion.Validator/Validators/EntidadGrupo/EntidadGrupoValidationManager.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation.Results;
+using PCM.SIP.ICP.Aplicacion.Dto;
+
+namespace PCM.SIP.ICP.Aplicacion.Validator
+{
+    public class EntidadGrupoValidationManager
+    {
+        private readonly EntidadGrupoIdRequestValidator _EntidadGrupoIdRequestValidator;
+        private readonly EntidadGrupoInsertRequestValidator _EntidadGrupoInsertRequestValidator;
+        private readonly EntidadGrupoUpdateRequestValidator _EntidadGrupoUpdateRequestValidator;
+
+        public EntidadGrupoValidationManager()
+        {
+            _EntidadGrupoIdRequestValidator = new EntidadGrupoIdRequestValidator();
+            _EntidadGrupoInsertRequestValidator = new EntidadGrupoInsertRequestValidator();
+            _EntidadGrupoUpdateRequestValidator = new EntidadGrupoUpdateRequestValidator();
+        }
+
+        public ValidationResult Validate(EntidadGrupoIdRequest entidad)
+        {
+            return _EntidadGrupoIdRequestValidator.Validate(entidad);
+        }
+        public ValidationResult Validate(EntidadGrupoInsertRequest entidad)
+        {
+            return _EntidadGrupoInsertRequestValidator.Validate(entidad);
+        }
+        public ValidationResult Validate(EntidadGrupoUpdateRequest entidad)
+        {
+            return _EntidadGrupoUpdateRequestValidator.Validate(entidad);
+        }
+    }
+}
---
+++ PCM.SIP.ICP/PCM.SIP.ICP.Aplicacion.Validator/Validators/EntidadGrupo/EntidadGrupoValidationRules.cs
@@ -0,0 +1,77 @@
+﻿using FluentValidation;
+using PCM.SIP.ICP.Aplicacion.Dto;
+
+namespace PCM.SIP.ICP.Aplicacion.Validator
+{
+    public class EntidadGrupoIdRequestValidator : AbstractValidator<EntidadGrupoIdRequest>
+    {
+        public EntidadGrupoIdRequestValidator()
+        {
+            RuleFor(u => u.SerialKey)
+                .IsNullOrWhiteSpace()
+                  .WithMessage("Debe ingresar el Id del EntidadGrupo");
+        }
+    }
+    public class EntidadGrupoInsertRequestValidator : AbstractValidator<EntidadGrupoInsertRequest>
+    {
+        public EntidadGrupoInsertRequestValidator()
+        {
+            RuleFor(u => u.codigo)
+                .IsNullOrWhiteSpace()
+                .WithMessage("Debe ingresar un codigo");
+
+            RuleFor(u => u.abreviatura)
+                .IsNullOrWhiteSpace()
+                .WithMessage("Debe ingresar una abreviatura");
+
+            RuleFor(u => u.descripcion)
+                .IsNullOrWhiteSpace()
+                .WithMessage("Debe ingresar una descripcion");
+
+            RuleFor(x => x.codigo)
+                .MaximumLength(10)
+                .WithMessage("El codigo debe tener máximo 10 caracteres");
+
+            RuleFor(x => x.abreviatura)
+               .MaximumLength(20)
+               .WithMessage("La abreviatura debe tener máximo 20 caracteres");
+
+            RuleFor(x => x.descripcion)
+                .MaximumLength(50)
+                .WithMessage("La descripcion debe tener máximo 50 caracteres");
+        }
+    }
+    public class EntidadGrupoUpdateRequestValidator : AbstractValidator<EntidadGrupoUpdateRequest>
+    {
+        public EntidadGrupoUpdateRequestValidator()
+        {
+            RuleFor(u => u.SerialKey)
+                .IsNullOrWhiteSpace()
+                  .WithMessage("Debe ingresar el Id del Tipo de documento");
+
+            RuleFor(u => u.codigo)
+                .IsNullOrWhiteSpace()
+                .WithMessage("Debe ingresar un codigo");
+
+            RuleFor(u => u.abreviatura)
+                .IsNullOrWhiteSpace()
+                .WithMessage("Debe ingresar una abreviatura");
+
+            RuleFor(u => u.descripcion)
+                .IsNullOrWhiteSpace()
+                .WithMessage("Debe ingresar una descripcion");
+
+            RuleFor(x => x.codigo)
+                .MaximumLength(10)
+                .WithMessage("El codigo debe tener máximo 10 caracteres");
+
+            RuleFor(x => x.abreviatura)
+               .MaximumLength(20)
+               .WithMessage("La abreviatura debe tener máximo 20 caracteres");
+
+            RuleFor(x => x.descripcion)
+                .MaximumLength(50)
+                .WithMessage("La descripcion debe tener máximo 50 caracteres");
+        }
+    }
+}
