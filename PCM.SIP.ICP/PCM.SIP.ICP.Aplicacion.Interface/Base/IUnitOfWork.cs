f8c303b7d7fd92590f92128f4f3bcbd1fe17e1fe
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-06-10 19:17:21 -0500

    Feat: Implement Application.Interface Project

---
+++ PCM.SIP.ICP/PCM.SIP.ICP.Aplicacion.Interface/Base/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using PCM.SIP.ICP.Aplicacion.Interface.Persistence;
+
+namespace PCM.SIP.ICP.Aplicacion.Interface
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IEntidadGrupoRepository EntidadGrupo { get; }
+    }
+}
